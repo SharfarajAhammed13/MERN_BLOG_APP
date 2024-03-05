ee08fca1220679e8dcbb321bb674cc2e0990cd1f
SHAIK SHARFARAJ AHAMMED <sharfaraj13@gmail.com>
2024-03-05 21:31:22 +0530

    SignUp API Complete

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    userName: {
+    username: {
         type: String,
         required: true,
         unique: true,
@@ -20,6 +20,6 @@ const userSchema = new mongoose.Schema({
 
 );
 
-const User = mongoose.model('User',userSchema);
+const User = mongoose.model('User', userSchema);
 
 export default User;
