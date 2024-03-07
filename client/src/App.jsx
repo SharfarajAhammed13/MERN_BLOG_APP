3d902616e5732f59fb8f146f8872597138289513
SHAIK SHARFARAJ AHAMMED <sharfaraj13@gmail.com>
2024-03-07 12:41:51 +0530

    Create Footer Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
@@ -7,6 +6,7 @@ import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import Header from './components/Header'
+import FooterComponet from './components/Footer'
 
 export default function App() {
   return (
@@ -20,6 +20,7 @@ export default function App() {
         <Route path='/dashboard' element={<Dashboard/>}/>
         <Route path='/projects' element={<Projects/>}/>
       </Routes>
+      <FooterComponet/>
     </BrowserRouter>
   )
 }
