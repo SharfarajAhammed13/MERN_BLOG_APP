d282115eea79265255ad754cb14b5e8af7af799c
SHAIK SHARFARAJ AHAMMED <sharfaraj13@gmail.com>
2024-03-09 11:46:25 +0530

    complete delete user account functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,16 +1,18 @@
 import { useSelector } from 'react-redux';
-import { Alert, Button, TextInput } from 'flowbite-react'
+import { Alert, Button, Modal, TextInput } from 'flowbite-react'
 import { useEffect, useRef, useState } from 'react';
+import {HiOutlineExclamationCircle} from 'react-icons/hi'
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import { updateStart, updateSuccess, updateFailure } from '../redux/user/userSlice.js'
+import { updateStart, updateSuccess, updateFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure } from '../redux/user/userSlice.js'
 import { useDispatch } from 'react-redux';
 import { app } from '../firebase';
 
 export default function DashProfile() {
-  const {currentUser} = useSelector(state => state.user)
+  const {currentUser, error} = useSelector(state => state.user)
   const [imageFile, setImageFile] = useState(null);
+  const [showModel, setShowModel] = useState(false);
   const [imageFileUploading, setImageFileUploading] = useState(false);
   const [updateUserSuccess,setUpdateUserSuccess] = useState(null);
   const [updateUserError, setUpdateUserError] = useState(null);
@@ -118,7 +120,25 @@ export default function DashProfile() {
       dispatch(updateFailure(error.message));
       setUpdateUserError(error.message);
     }
-  }
+  };
+
+  const handleDeleteUser = async () => {
+      setShowModel(false);
+      try {
+        dispatch(deleteUserStart());
+        const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+          method: 'DELETE',
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          dispatch(deleteUserFailure(data.message));
+        } else {
+          dispatch(deleteUserSuccess(data));
+        }
+      } catch (error) {
+        dispatch(deleteUserFailure(error.message));
+      }
+  };
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
       <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
@@ -186,7 +206,7 @@ export default function DashProfile() {
         </Button>
       </form>
       <div className="text-red-500 flex justify-between mt-5">
-        <span className='cursor-pointer'>Delete Account</span>
+        <span onClick={()=> setShowModel(true)}  className='cursor-pointer'>Delete Account</span>
         <span className='cursor-pointer'>Sign Out</span>
       </div>
       {updateUserSuccess && (
@@ -199,6 +219,24 @@ export default function DashProfile() {
         <Alert color='failure' className='mt-5'>{updateUserError}
         </Alert>
       )}
+      {error && (
+        <Alert color='failure' className='mt-5'>{error}
+        </Alert>
+      )}
+      <Modal show={showModel} onClose={()=> setShowModel(false)} popup size='md'>
+        <Modal.Header />
+          <Modal.Body>
+            <div className="text-center">
+              <HiOutlineExclamationCircle className='h-14 w-14 text-gray-500 dark:text-gray-200 mb-4 mx-auto '/>
+              <h3 className='mb-5 text-lg text-gray-500 drak:text-gray-400'>Are you sure you want to delete your account?</h3>
+              <div className='flex justify-center gap-4'>
+                <Button color='failure' onClick={handleDeleteUser}>Yes, I'm sure</Button>
+                <Button color='gray' onClick={()=> setShowModel(false)}>No, Cancel</Button>
+              </div>
+            </div>
+          </Modal.Body>
+        
+      </Modal>
     </div>
   );
 }
