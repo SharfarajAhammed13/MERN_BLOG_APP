53de31b784582946254205de8d0abe4aaeca2004
SHAIK SHARFARAJ AHAMMED <sharfaraj13@gmail.com>
2024-03-17 22:36:09 +0530

    add comments section to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import { Sidebar } from 'flowbite-react';
 import { useEffect, useState } from 'react';
-import { HiUser, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup } from 'react-icons/hi';
+import { HiUser, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiAnnotation } from 'react-icons/hi';
 import { Link, useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { signoutSuccess } from '../redux/user/userSlice';
@@ -43,17 +43,38 @@ export default function DashSidebar() {
                     Profile
                 </Sidebar.Item>
                 </Link>
-                
                 {currentUser.isAdmin && (
-                    <Link to='/dashboard?tab=posts'>
+                  <Link to='/dashboard?tab=posts'>
                     <Sidebar.Item
                       active={tab === 'posts'}
-                      as='div'
                       icon={HiDocumentText}
+                      as='div'
                     >
                       Posts
                     </Sidebar.Item>
                   </Link>
+                )}
+                {currentUser.isAdmin && (
+                    <>
+                    <Link to='/dashboard?tab=users'>
+                      <Sidebar.Item
+                        active={tab === 'users'}
+                        icon={HiOutlineUserGroup}
+                        as='div'
+                      >
+                        Users
+                      </Sidebar.Item>
+                    </Link>
+                    <Link to='/dashboard?tab=comments'>
+                      <Sidebar.Item
+                        active={tab === 'comments'}
+                        icon={HiAnnotation}
+                        as='div'
+                      >
+                        Comments
+                      </Sidebar.Item>
+                    </Link>
+                  </>
                 )}
                 {currentUser.isAdmin && (
                     <Link to='/dashboard?tab=users'>
